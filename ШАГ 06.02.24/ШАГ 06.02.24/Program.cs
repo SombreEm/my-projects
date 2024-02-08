f1bf13aa706c3a6b42291ec3679cded2ed660d99
SombreEm <133245293+SombreEm@users.noreply.github.com>
2024-02-08 20:56:13 +0200

    README

---
+++ ШАГ 06.02.24/ШАГ 06.02.24/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ШАГ_06._02._24
+{
+    public class Figure
+    {
+        public  double Area { get; }
+    }
+    public class Square : Figure
+    {
+        public double Side { get; set; }
+        public double Area => Side * Side;
+    }
+    public class Circle : Figure
+    {
+        public double Radius { get; set; }
+        public double Area => Math.PI * Math.Pow(Radius,2);
+    }
+    public class Animal
+    {
+        public string Name { get; set; }
+    }
+
+    public class Dog : Animal
+    {
+        public string Sound
+        {
+            get { return "ГАФ!"; }
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public string Sound
+        {
+            get { return "МЯУ!"; }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Square square = new Square { Side = 200 };
+            Circle circle = new Circle { Radius = 13 };
+
+            Console.WriteLine($"Квадрат - {square.Area}");
+            Console.WriteLine($"Коло - {circle.Area}");
+
+
+            Dog myDog = new Dog { Name = "Гріс" };
+            Cat myCat = new Cat { Name = "Мурзік" };
+
+            Console.WriteLine($"{myDog.Name} - {myDog.Sound}");
+            Console.WriteLine($"{myCat.Name} - {myCat.Sound}");
+        }
+    }
+}
---
+++ ШАГ 08.02.24/ШАГ 08.02.24/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ШАГ_08._02._24
+{
+    internal class Program
+    {
+        public static int MinIn2DArray(int[,] mas)
+        {
+            int minSum = 0;
+            for (int i = 0; i < mas.GetLength(0); i++)
+            {
+                int minInRow = mas[i, 0];
+                for (int j = 1; j < mas.GetLength(1); j++)
+                {
+                    minInRow = Math.Min(minInRow, mas[i, j]);
+                }
+                minSum += minInRow;
+            }
+            return minSum;
+        }
+
+        public static int MaxIn2DArray(int[,] mas)
+        {
+            int maxSum = 0;
+            for (int i = 0; i < mas.GetLength(0); i++)
+            {
+                int maxInRow = mas[i, 0];
+                for (int j = 1; j < mas.GetLength(1); j++)
+                {
+                    maxInRow = Math.Max(maxInRow, mas[i, j]);
+                }
+                maxSum += maxInRow;
+            }
+            return maxSum;
+        }
+
+        public static int DifferenceIn2DArray(int[,] mas)
+        {
+            return MaxIn2DArray(mas) - MinIn2DArray(mas);
+        }
+
+        public static void Main(string[] args)
+        {
+            int[,] number = {
+                {5, 3, 2, 4, 1},
+                {8, 10, 6, 9, 7},
+                {13, 15, 11, 14, 15},
+                {44, 55, 12, 10, 3}
+            };
+
+            Console.WriteLine($" {MinIn2DArray(number)} \n {MaxIn2DArray(number)} \n {DifferenceIn2DArray(number)}");
+        }
+    }
+}
+
