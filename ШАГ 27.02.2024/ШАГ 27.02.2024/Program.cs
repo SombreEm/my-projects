80f373fea24a8eb15964a2bae2f63fd6e7dbe794
SombreEm <133245293+SombreEm@users.noreply.github.com>
2024-02-27 20:47:51 +0200

    README

---
+++ ШАГ 27.02.2024/ШАГ 27.02.2024/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ШАГ_27._02._2024
+{
+    internal class Program
+    {
+        public delegate void TemperatureChangeHandler(int newTemperature);
+        public class TemperatureSensor
+        {
+            public event TemperatureChangeHandler TemperatureChanged;
+            private int currentTemperature;
+            public void UpdateTemperature(int newTemperature)
+            {
+                if (newTemperature != currentTemperature)
+                {
+                    currentTemperature = newTemperature;
+                    OnTemperatureChanged(newTemperature);
+                }
+            }
+            protected virtual void OnTemperatureChanged(int newTemperature)
+            {
+                TemperatureChanged.Invoke(newTemperature);
+            }
+        }
+        public class TemperatureDisplay
+        {
+            public void HandleTemperatureChange(int newTemperature)
+            {
+                Console.WriteLine($"Temperature changed: {newTemperature} degrees");
+            }
+        }
+        static void Main(string[] args)
+        {
+            TemperatureSensor sensor = new TemperatureSensor();
+            TemperatureDisplay display = new TemperatureDisplay();
+            sensor.TemperatureChanged += display.HandleTemperatureChange;
+            sensor.UpdateTemperature(25); 
+            sensor.UpdateTemperature(28);
+            sensor.UpdateTemperature(28);
+        }
+    }
+}
