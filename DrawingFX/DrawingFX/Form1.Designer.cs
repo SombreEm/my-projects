8ad1e4709870a26ab9420b35d1b4ac563427b042
SombreEm <133245293+SombreEm@users.noreply.github.com>
2024-07-02 14:16:57 +0300

    README

---
+++ DrawingFX/DrawingFX/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace DrawingFX
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Create = new System.Windows.Forms.Button();
+            this.inputValue = new System.Windows.Forms.TextBox();
+            this.Clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Create
+            // 
+            this.Create.Location = new System.Drawing.Point(12, 38);
+            this.Create.Name = "Create";
+            this.Create.Size = new System.Drawing.Size(100, 23);
+            this.Create.TabIndex = 0;
+            this.Create.Text = "Create";
+            this.Create.UseVisualStyleBackColor = true;
+            this.Create.Click += new System.EventHandler(this.Create_Click);
+            // 
+            // inputValue
+            // 
+            this.inputValue.Location = new System.Drawing.Point(12, 12);
+            this.inputValue.Name = "inputValue";
+            this.inputValue.Size = new System.Drawing.Size(100, 20);
+            this.inputValue.TabIndex = 1;
+            this.inputValue.Text = "2";
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(12, 67);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(100, 23);
+            this.Clear.TabIndex = 2;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 861);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.inputValue);
+            this.Controls.Add(this.Create);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResizeEnd += new System.EventHandler(this.Create_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Create;
+        private System.Windows.Forms.TextBox inputValue;
+        private System.Windows.Forms.Button Clear;
+    }
+}
+
---
+++ DrawingFX/DrawingFX/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DrawingFX
+{
+    public partial class Form1 : Form
+    {
+        class Line
+        {
+            public List<Point> points = new List<Point>();
+            public Line(string type, Form form) {
+                
+                if (type == "horizontal") {
+                    points.Add(new Point(0, form.Size.Height / 2));
+                    points.Add(new Point(form.Size.Width,form.Size.Height / 2));
+                }
+                else if (type == "vertical") {
+                    points.Add(new Point(form.Size.Width / 2, 0));
+                    points.Add(new Point(form.Size.Width / 2, form.Size.Height));
+                }
+            }
+            public Point returnPoint(int index)
+            {
+                return points[index];
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            this.BackColor = Color.White;
+        }
+
+        private void Create_Click(object sender, EventArgs e)
+        {
+            Graphics graphics = CreateGraphics();
+            graphics.Clear(Color.White);
+            Pen pen = new Pen(Color.Black);
+            Line horizontal = new Line("horizontal",this),
+                 vertical = new Line("vertical",this);
+            graphics.DrawLine(pen, horizontal.returnPoint(0),
+                                   horizontal.returnPoint(1));
+            graphics.DrawLine(pen, vertical.returnPoint(0),
+                                   vertical.returnPoint(1));
+
+            Point Center = new Point(this.Size.Width/2, this.Size.Height/2);
+
+            double x = Convert.ToDouble(inputValue.Text);
+            double y = Convert.ToDouble(Math.Pow(x,2));
+            Point Stream = Center;
+            Point superCenter = new Point(this.Size.Width / 2, this.Size.Height / 2);
+            int step = 2;
+            for (int i = 0; i < 400; i++)
+            {
+                graphics.DrawLine(pen, Stream, new Point((Stream.X + Convert.ToInt32(x)* 5), (Stream.Y - Convert.ToInt32(y) * 5)));
+                Stream = new Point((Stream.X + Convert.ToInt32(x) * 5), (Stream.Y - Convert.ToInt32(y) * 5));
+                x++;
+                y = x * x;
+            }
+            x = Convert.ToDouble(inputValue.Text);
+            y = Convert.ToDouble(Math.Pow(x, 2));
+            Stream = Center;
+            
+            for (int i = 0; i < 400; i++)
+            {
+                graphics.DrawLine(pen, Stream, new Point((Stream.X - Convert.ToInt32(x) * 5), (Stream.Y - Convert.ToInt32(y) * 5)));
+                Stream = new Point((Stream.X - Convert.ToInt32(x) * 5), (Stream.Y - Convert.ToInt32(y) * 5));
+                x++;
+                y = x * x;
+            }
+            graphics.Dispose();
+            pen.Dispose();
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            Graphics graphics = CreateGraphics();
+            graphics.Clear(Color.White);
+        }
+    }
+}
