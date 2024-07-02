f14706d2a0283fe725205b96ad35a548fc55d71c
SombreEm <133245293+SombreEm@users.noreply.github.com>
2024-07-02 14:26:52 +0300

    README

---
+++ Animation/Animation/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Animation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            //this.sizeBox = new System.Windows.Forms.TextBox();
+            this.Start = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // sizeBox
+            // 
+            //this.sizeBox.Location = new System.Drawing.Point(13, 13);
+            //this.sizeBox.Name = "sizeBox";
+            //this.sizeBox.Size = new System.Drawing.Size(100, 20);
+            //this.sizeBox.TabIndex = 0;
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(119, 11);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(75, 23);
+            this.Start.TabIndex = 1;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(200, 11);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(75, 23);
+            this.Stop.TabIndex = 2;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Start);
+            //this.Controls.Add(this.sizeBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox size;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button Stop;
+    }
+}
+
---
+++ Animation/Animation/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Animation
+{
+    public partial class Form1 : Form
+    {
+        Point start, end;
+        int value = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            start = new Point(0, Height / 2);
+            end = start;
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+
+            CreateGraphics().Clear(Color.White);
+            CreateGraphics().DrawLine(new Pen(Color.Black), new Point(Width / 2, 0), new Point(Width / 2, Height));
+            CreateGraphics().DrawLine(new Pen(Color.Black), new Point(0, Height / 2), new Point(Width, Height / 2));
+
+            timer1.Start();
+
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+
+            start = new Point(0, Height / 2);
+            end = start;
+            timer1.Stop();
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            end.X += 2;
+            end.Y -= Convert.ToInt32(Math.Sin(value) * 10);
+            value++;
+            CreateGraphics().DrawLine(new Pen(Color.Black), start, end);
+            start = end;
+
+            if (end.X > Width)
+            {
+                timer1.Stop();
+            }
+
+        }
+    }
+}
